fc4e324d3d9ef367e659ff5867e51a55e64b637a
vagrant <vagrant@homestead>
2018-12-28 01:42:21 +0000

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,8 +16,8 @@
 
             {{-- 右边部分 --}}
             <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登陆</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
             </ul>
         </div>
     </div>
