a803259ca9491816e8016217096b65282a13b19b
vagrant <vagrant@homestead>
2018-12-29 07:55:40 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     //用户个人中心页面
     public function show(User $user){
         return view('users.show',compact('user'));
@@ -16,11 +20,13 @@ class UsersController extends Controller
 
     //用户编辑资料页面
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     //用户修改资料逻辑
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $this->authorize('update',$user);
         $data = $request->all();
         if($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id,416);
@@ -28,7 +34,6 @@ class UsersController extends Controller
                 $data['avatar'] = $result['path'];
             }
         }
-
         $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
