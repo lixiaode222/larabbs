fb303befcacb0d27c1b9a89bac9322549a1e22b9
vagrant <vagrant@homestead>
2018-12-29 03:02:35 +0000

    用户个人页面原型

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +36,13 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    //重写修改密码逻辑 增加成功时的闪存信息
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success','密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath())
+            ->with('status', trans($response));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+//用户相关路由
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+
