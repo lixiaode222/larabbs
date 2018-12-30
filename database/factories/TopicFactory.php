15a01af2345adb59250b42d5a1dd02f2dde421ba
vagrant <vagrant@homestead>
2018-12-30 01:51:14 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    //随机选取这一个月的时间,作为更新时间
+    $updated_at = $faker->dateTimeThisMonth();
+    //在这个月里随机选个时间作为创建时间，但要比更新时间早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\Category;
+use App\Models\User;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        //所有用户ID数组
+        $user_ids = User::all()->pluck('id')->toArray();
 
+        //所有分类ID数组
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        //获取Faker实例
+        $faker = app(Faker\Generator::class);
+
+        //生成的话题假数据
+        $topics = factory(Topic::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($topic,$index)use($user_ids,$category_ids,$faker){
+
+                            //从用户ID数组中随机取一个作为话题的用户ID
+                            $topic->user_id = $faker->randomElement($user_ids);
+
+                            //从分类ID数组中随机取一个作为话题的分类
+                            $topic->category_id = $faker->randomElement($category_ids);
+                        });
+
+        //将数据转为数组格式，并插入到数据库中
         Topic::insert($topics->toArray());
     }
 
